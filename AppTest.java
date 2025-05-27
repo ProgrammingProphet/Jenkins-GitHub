318da923eff60b7d81e5419e411b53ed2f867533
ADITYA SHIVSHANKAR VISHWAKARMA <133384442+ProgrammingProphet@users.noreply.github.com>
2025-05-27 12:31:43 +0530

    Update and rename test_app.py to AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+        assertEquals(0, App.add(-1, 1));
+    }
+}
